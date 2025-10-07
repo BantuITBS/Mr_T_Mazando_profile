077969ee71b2e422d8baddaca24c989122810aa1
BantuITBS <bantuitbs@gmail.com>
2025-10-07 18:40:58 +0200

    Deploy updated Flutter web app with corrected index.html

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -39,7 +39,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ea9b83b18543f78487bfae32813d1986
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "c542ab12930049b718985aa605b77b51",
+"flutter_bootstrap.js": "c5c5d8b99fad3099e2de41dff66536c7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
